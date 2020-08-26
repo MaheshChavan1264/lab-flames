880f02cb8edfe5910354295a0a3cb7717b75db32
Mahesh Tatyasaheb Chavan <mahesh.chavan1264@gmail.com>
2020-08-26 17:05:13 +0530

    ProGrad ID:1425684 -  Completed all the error and test cases fully

--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -7,3 +7,44 @@ package service;
 // You must return only the following values ['f','l','a','m','e','s']
 // change the return value at the end of the method corresponding to your return value
 
+public class FlamesCheckService{
+	public char findFlames(String name1, String name2){
+		StringBuffer s1=new StringBuffer(name1);
+		StringBuffer s2=new StringBuffer(name2);
+		int a=s1.length();
+		int b=s2.length();				
+			for(int i=0;i<a;i++)
+			{
+				char c=s1.charAt(i);
+				for(int j=0;j<b;j++)
+				{
+					char d=s2.charAt(j);
+					if(c==d)
+					{ 
+						s1.deleteCharAt(i);
+						s2.deleteCharAt(j);
+						a=s1.length();
+						b=s2.length();
+						i=0;
+						j=0;
+					}
+				}
+			}
+
+			int len = a+b;  
+			int e = len -1;    
+			StringBuffer s3 = new StringBuffer("flames");
+			int len3 = s3.length();
+			for(int k =0; k<5; k++) {   
+				if(e >= len3) { 
+					e = e % len3;
+				}
+					s3.deleteCharAt(e);
+					len3 = s3.length();
+					e = e+len-1;
+			}
+
+		 char result=s3.charAt(0);
+		 return result;	
+	}
+}
